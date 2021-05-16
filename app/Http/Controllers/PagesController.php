ec4eeccc3ebdf61e2e1896fac5939e087791f1ca
Denis <den4er@mail.ru>
2021-05-16 12:17:43 +0300

    output from bd

--- app/Http/Controllers/PagesController.php
+++ app/Http/Controllers/PagesController.php
@@ -2,7 +2,9 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Todo;
 use Illuminate\Http\Request;
+use Illuminate\Support\Str;
 
 class PagesController extends Controller
 {
@@ -17,8 +19,21 @@ class PagesController extends Controller
     // deals
     public function todosPage()
     {
+        /*
+        * Добавление записи в таблицу при обновлении страницы
+        *
+        *
+        $todo = Todo::create([
+           'title' => 'Прочитать книгу',
+            'note' => 'Начиная со стр 66',
+        ]);
+        */
+
+        $todos = Todo::All();
+
         return view('todos', [
            'title' => 'Список дел',
+            'todos' => $todos,
         ]);
     }
 }
--- resources/views/todos.blade.php
+++ resources/views/todos.blade.php
@@ -6,13 +6,35 @@
         <title>{{ $title }}</title>
         <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-+0n0xVW2eSR5OomGNYDnhzAbDsOXxcvSN1TPprVMTNDbiYZCxYbOOl7+AMvyTG2x" crossorigin="anonymous">
         <style>
             body {
                 font-family: 'Nunito', sans-serif;
+            }
+            .list-group{
+                max-width: 800px;
+                margin: 20px;
             }
         </style>
     </head>
     <body class="antialiased">
         <h1>{{ $title }}</h1>
+
+        <ul class="list-group">
+            @foreach($todos as $todo)
+            <li class="list-group-item d-flex justify-content-between align-items-center">
+                <div>
+                    {{ $todo->title }}
+                    <p style="font-size: 12px; color: #6d6d6d;">{{ $todo->note }}</p>
+                </div>
+                @if($todo->status)
+                    <span class="alert alert-success">Выполнено</span>
+                @else
+                    <span class="alert alert-danger">Не выполнено</span>
+                @endif
+            </li>
+            @endforeach
+        </ul>
+
     </body>
 </html>
